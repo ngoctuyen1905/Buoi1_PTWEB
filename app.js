2cd98fa7e7545cf86c2eccb7558d3535f94ceb3b
B1910018 <tuyenb1910018@student.ctu.edu.vn>
2022-09-16 22:06:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
 
