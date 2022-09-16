6e4c8a6c38ede8e4293c01e47293f94cca580293
B1910018 <tuyenb1910018@student.ctu.edu.vn>
2022-09-16 22:19:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/app/api-error");
 const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
-app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.json({ message: "welcome to contact book application" });
 });
-
+app.use("/api/contacts", contactsRouter);
+    //handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
